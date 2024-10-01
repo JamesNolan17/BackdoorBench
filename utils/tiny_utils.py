8367991a5e3c946ef2a5eb60f066bb090b945665
JamesNolan17 <vip17jamesnolan@gmail.com>
2024-10-01 16:52:38 +0800

    add blacklist gpu function.

--- utils/tiny_utils.py
+++ utils/tiny_utils.py
@@ -11,13 +11,18 @@ def set_info_logger():
     return logger
 
 def find_free_gpu(logger):
-    #if torch.cuda.is_available():
+    blacklist=[3]
     smi_output = subprocess.run(['nvidia-smi', '--query-gpu=memory.free', '--format=csv,nounits,noheader'], capture_output=True, text=True)
     free_memory = [int(x) for x in smi_output.stdout.strip().split('\n')]
-    most_free_gpu = free_memory.index(max(free_memory))
-    #device = torch.device(f"cuda:{most_free_gpu}")
-    logger.info(f"Using GPU: {most_free_gpu}, free memory: {int(free_memory[most_free_gpu]/1024)} GiB")
-    #else:
-    #    device = torch.device("cpu")
-    #    logger.info("Using CPU")
+    
+    # Filter out blacklisted GPUs
+    available_gpus = [(idx, mem) for idx, mem in enumerate(free_memory) if idx not in blacklist]
+    
+    if available_gpus:
+        most_free_gpu = max(available_gpus, key=lambda x: x[1])[0]
+        logger.info(f"Using GPU: {most_free_gpu}, free memory: {int(free_memory[most_free_gpu]/1024)} GiB")
+    else:
+        logger.error("No available GPUs found that are not blacklisted")
+        return None
+    
     return most_free_gpu
